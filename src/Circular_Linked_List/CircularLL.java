817501947aded3ebb8cdcf57dfcadd3a073a3d1f
Vulka9n <you@example.com>
2021-05-28 11:30:56 +0530

    Circular LL complete

--- src/Circular_Linked_List/CircularLL.java
+++ src/Circular_Linked_List/CircularLL.java
@@ -43,4 +43,56 @@ class CircularLL {
         }
         size++;
     }
+
+    void traverseCircularLL(){
+        Node tempNode = head;
+        // System.out.println(size);
+        for(int i = 0;i<size;i++){
+            System.out.print(tempNode.value);
+            tempNode = tempNode.next;
+            if(i!=size-1){
+                System.out.print("->");
+            }
+            
+        }
+        System.out.print("->"+tempNode.value);
+        System.out.println('\n');
+    }
+
+    void searchCircularLL(int nodevalue){
+        Node tempNode = head;
+        for(int i = 0;i<size;i++){
+            if(tempNode.value==nodevalue){
+                System.out.println("Value found at position : "+(i+1));
+                break;
+            }
+            tempNode = tempNode.next;
+        }
+        System.out.println();
+    }
+    void deleteCircularLL(int position){
+        Node tempNode = head;
+        Node nextNode = head;
+        if(position==0){
+            System.out.println("Enter valid position");
+        }
+        else if(position>=size){
+            for(int i = 0;i<size-1;i++){
+                tempNode = tempNode.next;
+            }
+            tempNode.next = head;
+            tail.next = null;
+            tail = tempNode;
+        }
+        else{
+            for(int i = 0;i<position;i++){
+                nextNode = nextNode.next;
+            }
+            for(int i = 0;i<position-2;i++){
+                tempNode = tempNode.next;
+            }
+            tempNode.next = nextNode;
+        }
+        size--;
+    }
 }
--- src/Circular_Linked_List/Main.java
+++ src/Circular_Linked_List/Main.java
@@ -6,8 +6,14 @@ public class Main {
         cll.createCircularLL(5);
         // System.out.println(cll.head.value);
         // System.out.println(cll.head.next.value);
-        cll.insertCircularLL(6,1);   
-        System.out.println(cll.head.value);
-        System.out.println(cll.tail.value);
+        cll.insertCircularLL(6,2);
+        cll.insertCircularLL(7,3);
+        cll.insertCircularLL(8,4);   
+        // System.out.println(cll.head.value);
+        // System.out.println(cll.tail.value);
+        cll.traverseCircularLL();
+        cll.searchCircularLL(7);
+        cll.deleteCircularLL(3);
+        cll.traverseCircularLL();
     }
 }
