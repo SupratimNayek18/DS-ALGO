319d8c544d32aac69d7ab33720810c9b84e12d75
Supratim Nayek <supratimnayek@gmail.com>
2021-07-01 00:36:24 +0530

    Binary Heap Done

--- src/Binary_Heap/BinaryHeap.java
+++ src/Binary_Heap/BinaryHeap.java
@@ -23,4 +23,111 @@ public class BinaryHeap {
             return arr[1];
         }
     }
+
+    int sizeOfHeap(){
+        return sizeOfTree;
+    }
+
+    void levelOrderTraversal(){
+        for(int i=1;i<=sizeOfTree;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    void heapifyBottomToTop(int index,String heapType){
+        int parent = index/2;
+        if(index<=1){
+            return;
+        }
+        if(heapType=="min"){
+            if(arr[index]<arr[parent]){
+                int temp = arr[index];
+                arr[index] = arr[parent];
+                arr[parent] = temp;
+            }
+        }else if(heapType=="max"){
+            if(arr[index]>arr[parent]){
+                int temp = arr[index];
+                arr[index] = arr[parent];
+                arr[parent] = temp;
+            }
+        }
+        heapifyBottomToTop(parent,heapType);
+    }
+
+    void insert(int value,String heapType){
+        arr[sizeOfTree+1] = value;
+        sizeOfTree++;
+        heapifyBottomToTop(sizeOfTree,heapType);
+        System.out.println(value+"successfully insert in heap");  
+    }
+
+    void heapifyTopToBottom(int index,String heapType){
+        int leftChild = index*2;
+        int rightChild = index*2+1;
+        int swapChild=0;
+        if(sizeOfTree<leftChild){
+            return;
+        }
+        if(heapType=="max"){
+            if(sizeOfTree==leftChild){
+                if(arr[index]<arr[leftChild]){
+                    int temp = arr[index];
+                    arr[index] = arr[leftChild];
+                    arr[leftChild] = temp;
+                }
+                return;
+            }else{
+                if(arr[leftChild]>arr[rightChild]){
+                    swapChild = leftChild;
+                }else{
+                    swapChild = rightChild;
+                }
+                if(arr[index]<arr[swapChild]){
+                    int temp = arr[index];
+                    arr[index] = arr[swapChild];
+                    arr[swapChild] = temp;
+                }
+            }
+        }else if(heapType=="min"){
+            if(sizeOfTree==leftChild){
+                if(arr[index]>arr[leftChild]){
+                    int temp = arr[index];
+                    arr[index] = arr[leftChild];
+                    arr[leftChild] = temp;
+                }
+                return;
+            }else{
+                if(arr[leftChild]>arr[rightChild]){
+                    swapChild = rightChild;
+                }else{
+                    swapChild = leftChild;
+                }
+                if(arr[index]>arr[swapChild]){
+                    int temp = arr[index];
+                    arr[index] = arr[swapChild];
+                    arr[swapChild] = temp;
+                }
+            }
+        }
+        heapifyTopToBottom(swapChild,heapType);
+    }
+
+    int extractHeadOfHeap(String heapType){
+        if(isEmpty()) return -1;
+        else{
+            int extractedValue = arr[1];
+            arr[1] = arr[sizeOfTree];
+            sizeOfTree--;
+            heapifyTopToBottom(1,heapType);
+            return extractedValue;
+        }
+    }
+
+    void deleteHeap(){
+        arr = null;
+        System.out.println("Heap deleted successfully");
+    }
+
 }
--- src/Binary_Heap/Main.java
+++ src/Binary_Heap/Main.java
@@ -3,6 +3,12 @@ package Binary_Heap;
 public class Main {
     public static void main(String[] args) {
         BinaryHeap bh = new BinaryHeap(8);
-        bh.peek();
+        bh.insert(10,"max");
+        bh.insert(5,"max");
+        bh.insert(15,"max");
+        bh.insert(1,"max");
+        bh.levelOrderTraversal();
+        bh.extractHeadOfHeap("max");
+        bh.levelOrderTraversal();
     }
 }
