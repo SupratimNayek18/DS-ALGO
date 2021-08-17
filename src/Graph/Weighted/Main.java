0850af3899db75f3564c6d31b2a66e56fa698403
Supratim Nayek <supratimnayek@gmail.com>
2021-08-17 20:05:23 +0530

    Floyd Warshall Done

--- src/Graph/Weighted/Main.java
+++ src/Graph/Weighted/Main.java
@@ -9,26 +9,36 @@ public class Main {
         nodeList.add(new WeightedNode("B",1));
         nodeList.add(new WeightedNode("C",2));
         nodeList.add(new WeightedNode("D",3));
-        nodeList.add(new WeightedNode("E",4));
-        nodeList.add(new WeightedNode("F",5));
-        nodeList.add(new WeightedNode("G",6));
+        // nodeList.add(new WeightedNode("E",4));
+        // nodeList.add(new WeightedNode("F",5));
+        // nodeList.add(new WeightedNode("G",6));
 
         WeightedGraph g = new WeightedGraph(nodeList);
 
-        g.addWeightedEdge(0, 1, 2);
-        g.addWeightedEdge(0, 2, 5);
-        g.addWeightedEdge(1, 2, 6);
-        g.addWeightedEdge(1, 3, 1);
-        g.addWeightedEdge(1, 4, 3);
-        g.addWeightedEdge(2, 5, 8);
-        g.addWeightedEdge(3, 4, 4);
-        g.addWeightedEdge(4, 6, 9);
-        g.addWeightedEdge(5, 6, 7);
+        // g.addWeightedEdge(0, 1, 2);
+        // g.addWeightedEdge(0, 2, 5);
+        // g.addWeightedEdge(1, 2, 6);
+        // g.addWeightedEdge(1, 3, 1);
+        // g.addWeightedEdge(1, 4, 3);
+        // g.addWeightedEdge(2, 5, 8);
+        // g.addWeightedEdge(3, 4, 4);
+        // g.addWeightedEdge(4, 6, 9);
+        // g.addWeightedEdge(5, 6, 7);
+        
+        g.addWeightedEdge(0,3,1);
+        g.addWeightedEdge(0,1,8);
+        g.addWeightedEdge(1,2,1);
+        g.addWeightedEdge(2,0,4);
+        g.addWeightedEdge(3,1,2);
+        g.addWeightedEdge(3,2,9);
 
         // System.out.println("Printing Djijkstra from source : A");
         // g.dijkstra(nodeList.get(0));
 
-        System.out.println("Printing Bellman Ford from source : A");
-        g.bellmanFord(nodeList.get(0));
+        // System.out.println("Printing Bellman Ford from source : A");
+        // g.bellmanFord(nodeList.get(0));
+
+        System.out.println("Printing Floyd Warshall algorithm from source A: ");
+        g.floydWarshall();
     }
 }
